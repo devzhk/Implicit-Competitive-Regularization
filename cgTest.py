Fix conjugate gradient residual initial value

cgTest.py
@@ -1,9 +1,22 @@
 import torch
 import torch.nn as nn
 import torch.autograd as autograd
-import warnings
 
-import unittest
+
+def Hvp_vec(grad_vec, params, vec, retain_graph=False):
+    '''
+    return Hessian vector product
+    '''
+    grad_grad = autograd.grad(grad_vec, params, grad_outputs=vec, retain_graph=retain_graph,
+                              allow_unused=True)
+    grad_list = []
+    for i, p in enumerate(params):
+        if grad_grad[i] is None:
+            grad_list.append(torch.zeros_like(p))
+        else:
+            grad_list.append(grad_grad[i].contiguous().view(-1))
+    hvp = torch.cat(grad_list)
+    return hvp
 
 
 class NetD(nn.Module):
@@ -34,8 +47,52 @@ class NetG(nn.Module):
         self.net.bias.data = torch.Tensor([-4.0, 3.0])
 
 
-D_gt = NetD()
-D_test = NetD()
-G_gt = NetG()
-G_test = NetG()
+lr = 0.01
+factor = lr
+D = NetD()
+G = NetG()
 
+z = torch.tensor([2.0])
+real_x = torch.tensor([[3.0, 4.0]])
+loss = D(G(z)) - D(real_x)
+d_param = list(D.parameters())
+g_param = list(G.parameters())
+
+grad_g = autograd.grad(loss, g_param, create_graph=True, retain_graph=True)
+grad_g_vec = torch.cat([g.contiguous().view(-1) for g in grad_g])
+grad_d = autograd.grad(loss, d_param, create_graph=True, retain_graph=True)
+grad_d_vec = torch.cat([g.contiguous().view(-1) for g in grad_d])
+
+g_vec_d = grad_g_vec.clone().detach()
+d_vec_d = grad_d_vec.clone().detach()
+
+A_g = torch.tensor([[4.0 * factor, 0.0, 2.0 * factor, 0],
+                    [0.0, 4.0 * factor, 0.0, 2.0 * factor],
+                    [2.0 * factor, 0.0, factor, 0.0],
+                    [0.0, 2.0 * factor, 0.0, factor]])
+A_d = torch.tensor([[5.0 * factor, 0.0, 0.0],
+                    [0.0, 5.0 * factor, 0.0],
+                    [0.0, 0.0, 0.0]])
+g_test = g_vec_d
+d_test = d_vec_d
+
+g_gt = g_vec_d.clone()
+d_gt = d_vec_d.clone()
+d_diffs = []
+g_diffs = []
+for i in range(1):
+    g_gt = torch.matmul(A_g, g_gt)
+    d_gt = torch.matmul(A_d, d_gt)
+
+    tmp1 = Hvp_vec(grad_d_vec, g_param, d_test, retain_graph=True).detach()
+    d_test = Hvp_vec(grad_g_vec, d_param, tmp1, retain_graph=True).detach().mul(factor)
+
+    tmp2 = Hvp_vec(grad_g_vec, d_param, g_test, retain_graph=True).detach()
+    g_test = Hvp_vec(grad_d_vec, g_param, tmp2, retain_graph=True).detach().mul(factor)
+    g_diffs.append((g_gt - g_test).tolist())
+    d_diffs.append((d_gt - d_test).tolist())
+
+print('%.3f * D_dg * D_gd * grad_d true value:' % factor)
+print(d_diffs)
+print('%.3f * D_gd * D_dg * grad_g :' % factor)
+print(g_diffs)

cgd_test.py
@@ -4,8 +4,8 @@ import unittest
 import torch
 import torch.nn as nn
 import torch.autograd as autograd
-from optims import ICR, BCGD, testBCGD, LCGD
-from optims.testOptim import testLCGD
+from optims import BCGD, LCGD
+from optims.testOptim import testLCGD, ICR, testBCGD
 from tensorboardX import SummaryWriter
 
 device = torch.device('cuda:0') if torch.cuda.is_available() else 'cpu'
@@ -74,14 +74,14 @@ def train():
 
 
 if __name__ == '__main__':
-    optim_type = 'ICR'
+    optim_type = 'testBCGD'
     lr = 0.1
-    epoch_num = 100
+    epoch_num = 50
     # device = torch.device('cuda:0') if torch.cuda.is_available() else 'cpu'
     device = 'cpu'
     D = NetD().to(device)
     G = NetG().to(device)
-    writer = SummaryWriter(log_dir='logs/test4/%s-r' % optim_type)
+    writer = SummaryWriter(log_dir='logs/test6/%s-real' % optim_type)
     if optim_type == 'ICR':
         optimizer = ICR(max_params=G.parameters(), min_params=D.parameters(),
                         lr=lr, alpha=1.0, device=device)
@@ -103,6 +103,9 @@ if __name__ == '__main__':
         loss = D(G(z)) - D(real_x)
         optimizer.zero_grad()
         optimizer.step(loss)
+        # if e == 1:
+        #     torch.save({'D': D.state_dict(),
+        #                 'G': G.state_dict()}, 'net.pth')
         writer.add_scalar('Generator/Weight0', G.net.weight.data[0].item(), global_step=e)
         writer.add_scalar('Generator/Weight1', G.net.weight.data[1].item(), global_step=e)
         writer.add_scalar('Generator/Bias0', G.net.bias.data[0].item(), global_step=e)

optims/cgd_utils.py
@@ -6,7 +6,7 @@ import warnings
 def conjugate_gradient(grad_x, grad_y,
                        x_params, y_params,
                        b, x=None, nsteps=10,
-                       tol=1e-20, atol=1e-32,
+                       tol=1e-12, atol=1e-20,
                        lr_x=1.0, lr_y=1.0,
                        device=torch.device('cpu')):
     """
@@ -25,21 +25,26 @@ def conjugate_gradient(grad_x, grad_y,
     """
     if x is None:
         x = torch.zeros(b.shape[0], device=device)
-    r = b.clone().detach()
+        r = b.clone().detach()
+    else:
+        h1 = Hvp_vec(grad_vec=grad_x, params=y_params, vec=x, retain_graph=True).detach_().mul(lr_y)
+        h2 = Hvp_vec(grad_vec=grad_y, params=x_params, vec=h1, retain_graph=True).detach_().mul(lr_x)
+        Avx = x + h2
+        r = b.clone().detach() - Avx
+
     p = r.clone().detach()
     rdotr = torch.dot(r, r)
     residual_tol = tol * rdotr
-    pArray = []
-    pArray.append(p)
+
     for i in range(nsteps):
         # To compute Avp
         h_1 = Hvp_vec(grad_vec=grad_x, params=y_params, vec=p, retain_graph=True).detach_().mul(lr_y)
         h_2 = Hvp_vec(grad_vec=grad_y, params=x_params, vec=h_1, retain_graph=True).detach_().mul(lr_x)
         Avp_ = p + h_2
-        pArray.append(Avp_)
 
         alpha = rdotr / torch.dot(p, Avp_)
         x.data.add_(alpha * p)
+
         r.data.add_(- alpha * Avp_)
         new_rdotr = torch.dot(r, r)
         beta = new_rdotr / rdotr
@@ -49,31 +54,13 @@ def conjugate_gradient(grad_x, grad_y,
             break
     if i > 99:
         warnings.warn('CG iter num: %d' % (i + 1))
-    return x, i + 1, pArray
+    return x, i + 1
 
 
 def Hvp(grad_vec, params, vec, retain_graph=False):
     grad_grad = autograd.grad(grad_vec, params, grad_outputs=vec, retain_graph=retain_graph)
     # hvp = torch.cat([g.contiguous().view(-1) for g in grad_grad])
     return grad_grad
-
-
-def Hvpvec(grad_vec, params, vec, retain_graph=False):
-    try:
-        grad_grad = autograd.grad(grad_vec, params.parameters(), grad_outputs=vec,
-                                  retain_graph=retain_graph)
-        hvp = torch.cat([g.contiguous().view(-1) for g in grad_grad])
-    except:
-        grad_grad = autograd.grad(grad_vec, params.parameters(), grad_outputs=vec,
-                                  retain_graph=retain_graph, allow_unused=True)
-        grad_list = []
-        for i, p in enumerate(params.parameters()):
-            if grad_grad[i] is None:
-                grad_list.append(torch.zeros_like(p))
-            else:
-                grad_list.append(grad_grad[i].contiguous().view(-1))
-        hvp = torch.cat(grad_list)
-    return hvp
 
 
 def Hvp_vec(grad_vec, params, vec, retain_graph=False):
@@ -84,7 +71,7 @@ def Hvp_vec(grad_vec, params, vec, retain_graph=False):
         raise ValueError('Gradvec nan')
     if torch.isnan(vec).any():
         raise ValueError('vector nan')
-        # print('filling zero for None')
+        # zero padding for None
     grad_grad = autograd.grad(grad_vec, params, grad_outputs=vec, retain_graph=retain_graph,
                               allow_unused=True)
     grad_list = []
@@ -155,64 +142,9 @@ def general_conjugate_gradient(grad_x, grad_y,
     return x, i + 1
 
 
-def mgeneral_conjugate_gradient(grad_x, grad_y,
-                                x_params, y_params, b,
-                                lr_x, lr_y, x=None,
-                                nsteps=10, tol=1e-12, atol=1e-20,
-                                device=torch.device('cpu')):
-    '''
-
-    :param grad_x:
-    :param grad_y:
-    :param x_params:
-    :param y_params:
-    :param b:
-    :param lr_x:
-    :param lr_y:
-    :param x:
-    :param nsteps:
-    :param residual_tol:
-    :param device:
-    :return: (I + sqrt(lr_x) * D_xy * lr_y * D_yx * sqrt(lr_x)) ** -1 * b
-
-    '''
-    if x is None:
-        x = torch.zeros(b.shape[0], device=device)
-    if grad_x.shape != b.shape:
-        raise RuntimeError('CG: hessian vector product shape mismatch')
-    lr_x = lr_x.sqrt()
-    r = b.clone().detach()
-    p = r.clone().detach()
-    rdotr = torch.dot(r, r)
-    residual_tol = tol * rdotr
-    for i in range(nsteps):
-        # To compute Avp
-        # h_1 = Hvp_vec(grad_vec=grad_x, params=y_params, vec=lr_x * p, retain_graph=True)
-        h_1 = Hvpvec(grad_vec=grad_x, params=y_params, vec=lr_x * p, retain_graph=True).mul_(lr_y)
-        # h_1.mul_(lr_y)
-        # lr_y * D_yx * b
-        # h_2 = Hvp_vec(grad_vec=grad_y, params=x_params, vec=lr_y * h_1, retain_graph=True)
-        h_2 = Hvpvec(grad_vec=grad_y, params=x_params, vec=h_1, retain_graph=True).mul_(lr_x)
-        # h_2.mul_(lr_x)
-        # lr_x * D_xy * lr_y * D_yx * b
-        Avp_ = p + h_2
-
-        alpha = rdotr / torch.dot(p, Avp_)
-        x.data.add_(alpha * p)
-        r.data.add_(- alpha * Avp_)
-        new_rdotr = torch.dot(r, r)
-        beta = new_rdotr / rdotr
-        p = r + beta * p
-        rdotr = new_rdotr
-        if rdotr < residual_tol or rdotr < atol:
-            break
-    if i > 99:
-        print('Warning: CG iteration number: %d' % (i + 1))
-    return x, i + 1
-
-
 def zero_grad(params):
     for p in params:
         if p.grad is not None:
             p.grad.detach()
             p.grad.zero_()
+

optims/testOptim.py
@@ -1,5 +1,101 @@
 import torch
 from .cgd_utils import zero_grad
+import matplotlib.pyplot as plt
+import torch.autograd as autograd
+
+plt.switch_backend('agg')
+import numpy as np
+import os
+from optims.cgd_utils import conjugate_gradient, Hvp_vec, zero_grad
+from scipy.sparse.linalg import cg
+
+
+class ICR(object):
+    def __init__(self, max_params, min_params,
+                 lr=1e-3, momentum=0, alpha=1.0,
+                 device=torch.device('cpu'), collect_info=True):
+        self.max_params = list(max_params)
+        self.min_params = list(min_params)
+        self.state = {'lr': lr, 'momentum': momentum, 'alpha': alpha,
+                      'step': 0, 'old_max': None, 'old_min': None,  # start point of CG
+                      'exp_avg_max': 0.0, 'exp_avg_min': 0.0}  # save last update
+        self.info = {'grad_x': None, 'grad_y': None,
+                     'hvp_x': None, 'hvp_y': None,
+                     'cg_x': None, 'cg_y': None,
+                     'time': 0, 'iter_num': 0}
+        self.device = device
+        self.collect_info = collect_info
+
+    def zero_grad(self):
+        zero_grad(self.max_params)
+        zero_grad(self.min_params)
+
+    def state_dict(self):
+        return self.state
+
+    def load_state_dict(self, state_dict):
+        self.state.update(state_dict)
+
+    def set_state(self, new_state):
+        self.state.update(new_state)
+        print('Current state: {}'.format(new_state))
+
+    def get_info(self):
+        if self.info['grad_x'] is None:
+            print('Warning! No update information stored. Set collect_info=True before call this method')
+        return self.info
+
+    def step(self, loss):
+        lr = self.state['lr']
+        alpha = self.state['alpha']
+        time_step = self.state['step'] + 1
+        self.state['step'] = time_step
+
+        grad_x = autograd.grad(loss, self.max_params, create_graph=True, retain_graph=True)
+        grad_x_vec = torch.cat([g.contiguous().view(-1) for g in grad_x])
+        grad_y = autograd.grad(loss, self.min_params, create_graph=True, retain_graph=True)
+        grad_y_vec = torch.cat([g.contiguous().view(-1) for g in grad_y])
+        grad_x_vec_d = grad_x_vec.clone().detach()
+        grad_y_vec_d = grad_y_vec.clone().detach()
+        hvp_x_vec = Hvp_vec(grad_y_vec, self.max_params, grad_y_vec_d,
+                            retain_graph=True)  # h_xy * d_y
+        hvp_y_vec = Hvp_vec(grad_x_vec, self.min_params, grad_x_vec_d,
+                            retain_graph=True)  # h_yx * d_x
+        p_x = torch.add(grad_x_vec_d, - lr * alpha * hvp_x_vec).detach_()
+        p_y = torch.add(grad_y_vec_d, lr * alpha * hvp_y_vec).detach_()
+
+        cg_x, x_i = conjugate_gradient(grad_x=grad_x_vec, grad_y=grad_y_vec,
+                                       x_params=self.max_params, y_params=self.min_params,
+                                       b=p_x, x=self.state['old_max'],
+                                       nsteps=p_x.shape[0], lr_x=lr, lr_y=alpha * lr,
+                                       device=self.device)
+        cg_x.detach_()
+        cg_y, y_i = conjugate_gradient(grad_x=grad_y_vec, grad_y=grad_x_vec,
+                                       x_params=self.min_params, y_params=self.max_params,
+                                       b=p_y, x=self.state['old_min'],
+                                       nsteps=p_y.shape[0], lr_x=lr, lr_y=alpha * lr,
+                                       device=self.device)
+        cg_y.detach_()
+
+        # path = 'figs/ICR/'
+        # if not os.path.exists(path):
+        #     os.makedirs(path)
+        # plot_cg(tensorList=g_array, name='%sgenerator_%d' % (path, time_step))
+        # plot_cg(tensorList=d_array, name='%sdiscriminator_%d' % (path, time_step))
+        self.state.update({'old_max': cg_x, 'old_min': cg_y})
+
+        index = 0
+        for p in self.max_params:
+            p.data.add_(lr * cg_x[index: index + p.numel()].reshape(p.shape))
+            index += p.numel()
+        if index != cg_x.numel():
+            raise ValueError('CG size mismatch')
+        index = 0
+        for p in self.min_params:
+            p.data.add_(- lr * cg_y[index: index + p.numel()].reshape(p.shape))
+            index += p.numel()
+        if index != cg_y.numel():
+            raise ValueError('CG size mismatch')
 
 
 class testBCGD(object):
@@ -11,6 +107,8 @@ class testBCGD(object):
         self.lr_max = lr_max
         self.lr_min = lr_min
         self.device = device
+        self.count = 0
+        self.state = {'old_g': None, 'old_d': None}
 
     def zero_grad(self):
         zero_grad(self.max_params)
@@ -39,8 +137,14 @@ class testBCGD(object):
         A_d = torch.tensor([[1.0 + 5.0 * factor, 0.0, 0.0],
                             [0.0, 1.0 + 5.0 * factor, 0.0],
                             [0.0, 0.0, 1.0]])
-        cg_g, iter_g = inverse(A=A_g, b=p_g, nsteps=p_g.shape[0])
-        cg_d, iter_d = inverse(A=A_d, b=p_d, nsteps=p_d.shape[0])
+        cg_g, iter_g, g_array = inverse(A=A_g, b=p_g, nsteps=p_g.shape[0])
+        cg_d, iter_d, d_array = inverse(A=A_d, b=p_d, nsteps=p_d.shape[0])
+        self.state.update({'old_g': cg_g, 'old_d': cg_d})
+        # path = 'figs/testBCGD/'
+        # if not os.path.exists(path):
+        #     os.makedirs(path)
+        # plot_cg(tensorList=g_array, name='%sgenerator_%d' % (path, self.count))
+        # plot_cg(tensorList=d_array, name='%sdiscriminator_%d' % (path, self.count))
         # cg_g, iter_g = inverse_gt(A=A_g, b=p_g)
         # cg_d, iter_d = inverse_gt(A=A_d, b=p_d)
         index = 0
@@ -48,11 +152,12 @@ class testBCGD(object):
             p.data.add_(self.lr_max * cg_g[index: index + p.numel()].reshape(p.shape))
             index += p.numel()
         assert index == cg_g.numel(), 'Maximizer CG size mismatch'
-        index =0
+        index = 0
         for p in self.min_params:
             p.data.add_(- self.lr_min * cg_d[index: index + p.numel()].reshape(p.shape))
             index += p.numel()
         assert index == cg_d.numel(), 'Minimizer CG size mismatch'
+        self.count += 1
 
 
 class testLCGD(object):
@@ -89,7 +194,7 @@ class testLCGD(object):
             p.data.add_(self.lr_max * cg_g[index: index + p.numel()].reshape(p.shape))
             index += p.numel()
         assert index == cg_g.numel(), 'Maximizer CG size mismatch'
-        index =0
+        index = 0
         for p in self.min_params:
             p.data.add_(- self.lr_min * cg_d[index: index + p.numel()].reshape(p.shape))
             index += p.numel()
@@ -110,12 +215,16 @@ def inverse(A, b, x=None, nsteps=10,
     rdotr = torch.dot(r, r)
     residual_tol = tol * rdotr
     pArray = []
-    pArray.append(p)
     for i in range(nsteps):
+        pArray.append(p)
+
         Avp_ = torch.matmul(A, p)
         pArray.append(Avp_)
+
         alpha = rdotr / torch.dot(p, Avp_)
         x.data.add_(alpha * p)
+        pArray.append(x.data)
+
         r.data.add_(- alpha * Avp_)
         new_rdotr = torch.dot(r, r)
         beta = new_rdotr / rdotr
@@ -129,3 +238,16 @@ def inverse(A, b, x=None, nsteps=10,
 def inverse_gt(A, b):
     return torch.matmul(torch.inverse(A), b), 0
 
+
+def plot_cg(tensorList, name):
+    array = []
+    for p in tensorList:
+        array.append(p.tolist())
+    array = np.array(array).transpose()
+    np.save('%s.npy' % name, array)
+    index = np.arange(array.shape[1])
+    fig, axs = plt.subplots(array.shape[0])
+    for i, a in enumerate(array):
+        axs[i].plot(index, a)
+        axs[i].set_title('Feature %d' % i)
+    plt.savefig('%s.png' % name)